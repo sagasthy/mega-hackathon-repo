4bccaa94083415736d86ccc59bf7e6a333235e67
surekhachillara <surekha.chillaraw@gmail.com>
2023-04-15 16:15:04 -0400

    error handling in stories api

--- backend/uwrite-rest-api/src/main/java/com/mega/uwrite/uwriterestapi/controller/StoryController.java
+++ backend/uwrite-rest-api/src/main/java/com/mega/uwrite/uwriterestapi/controller/StoryController.java
@@ -6,7 +6,10 @@ import com.mega.uwrite.uwriterestapi.model.Story;
 import com.mega.uwrite.uwriterestapi.model.User;
 import com.mega.uwrite.uwriterestapi.repository.StoryRepository;
 import com.mega.uwrite.uwriterestapi.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
@@ -27,25 +30,25 @@ public class StoryController {
     }
 
     @GetMapping("/get-list/{userId}")
-    public List<Story> getStoriesByUserId(@PathVariable Long userId) {
+    public ResponseEntity<List<Story>> getStoriesByUserId(@PathVariable Long userId) {
         Optional<User> user = userRepository.findById(userId);
 
         if(user.isEmpty()) throw new UserNotFoundException("Invalid User ID");
 
-        return user.get().getStories();
+        return ResponseEntity.ok().body(user.get().getStories());
     }
 
     @GetMapping("/get-story/{storyId}")
-    public Story getStoryById(@PathVariable Long storyId) {
+    public ResponseEntity<Story> getStoryById(@PathVariable Long storyId) {
         Optional<Story> story = storyRepository.findById(storyId);
 
         if(story.isEmpty()) throw new StoryNotFoundException("Invalid Story ID");
 
-        return story.get();
+        return ResponseEntity.ok().body(story.get());
     }
 
     @PutMapping("/new-story")
-    public Story createStory(@RequestBody StoryRequest storyRequest) {
+    public ResponseEntity<Story> createStory(@RequestBody StoryRequest storyRequest) {
         Optional<User> user = userRepository.findById(storyRequest.userId());
 
         if(user.isEmpty()) throw new UserNotFoundException("Invalid User ID");
@@ -59,7 +62,7 @@ public class StoryController {
 
         Story savedStory = storyRepository.save(story);
         userRepository.save(foundUser);
-        return savedStory;
+        return ResponseEntity.ok().body(savedStory);
     }
 }
 
---
+++ backend/uwrite-rest-api/src/main/java/com/mega/uwrite/uwriterestapi/exception/CustomExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.mega.uwrite.uwriterestapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = { UserNotFoundException.class, StoryNotFoundException.class })
+    protected ResponseEntity<ErrorResponse> handleException (
+            RuntimeException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse("NOT_FOUND", ex.getMessage(), LocalDateTime.now().toString());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
+
+record ErrorResponse (String errorCode, String errorDescription, String time) {}
