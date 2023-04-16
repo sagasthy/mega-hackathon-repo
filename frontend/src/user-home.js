65c3efa600cc1a2bb4bda54d06fb562c8e0c81ce
suryavegi <surya.vegi@yahoo.com>
2023-04-16 11:57:28 -0400

    added axios call

--- frontend/src/user-home.js
+++ frontend/src/user-home.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import { Container } from "react-bootstrap";
-import { Text } from "react-bootstrap-text";
-import "./css/SignupPage.css";
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 import Header from './header/header';
 
 const UserHomePage = () => {
-    return (
-        <>
-           <Header />
-            <Container>
-                <Text uppercase bold underline>This text is uppercase, bold and underlined</Text>
-            </Container>
-        </>
-    );
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8081/stories/get-list/user1')
+      .then(response => {
+        setStories(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <Row>
+        <Col key="1234" md={4}>
+              <div className="tile">                
+                <p>Dashboard</p>
+              </div>
+            </Col>
+          {stories.map(story => (
+            <Col key={story.id} md={4}>
+              <div className="tile">                
+                <p>{story.storyContent}</p>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default UserHomePage;
